5f54b3958a3f56e764b8e6bc37f5a917eda9dc7c
dmstj0162 <dmstj0162@gmail.com>
2026-05-12 16:54:51 +0900

    ãfeat: hasShield ê기능 추가

--- src/com/game/item/Item.java
+++ src/com/game/item/Item.java
@@ -5,5 +5,4 @@ import com.game.animal.Animal;
 public interface Item {
 
     default void run(Animal animal) {}
-
 }
--- src/com/game/item/Trap.java
+++ src/com/game/item/Trap.java
@@ -16,6 +16,12 @@ public class Trap implements Item {
             return;
         }
 
+        if(animal.hasShield()){
+            System.out.println(animal.getName() + "가 방어막을 사용하여 Trap을 막았습니다.");
+            animal.setHasShield(false);
+            return;
+        }
+            System.out.println("방어막이 없어서 Trap이 실행됩니다.");
             System.out.println(animal.getName() + "가 Trap(뒤로 1칸 가기)에 걸렸습니다.!");
             int presentPosition = animal.moveStep(-1);
             System.out.println("현재 " + presentPosition + "번째 칸 입니다.");
